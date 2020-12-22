9ebd2ea9fd9ac4f703c2f3b87a25f0f5c43bf217
Artem <rozen19942810@gmail.com>
2020-12-22 15:17:51 +0200

    add routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {LinksPage} from "./pages/LinksPage";
+import {CreatePage} from "./pages/CteatePage";
+import {DetailPage} from "./pages/DetailPage";
+import {AuthPage} from "./pages/AuthPage";
 
 export const useRoutes = isAuthenticated => {
     if (isAuthenticated) {
         return (
-            <div></div>
+            <Switch>
+                <Route path='/links' exact render={() => <LinksPage/>}/>
+                <Route path='/create' exact render={() => <CreatePage/>}/>
+                <Route path='/detail/:id' render={() => <DetailPage/>}/>
+                <Redirect to={'/create'}/>
+            </Switch>
         )
     }
+    return (
+        <Switch>
+            <Route path='/' exact render={() => <AuthPage/>}/>
+            <Redirect to={'/'}/>
+        </Switch>
+    )
 }
