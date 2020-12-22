5f8a45086a96d3ba72bfc45bf98d67c80d28ce85
Artem <rozen19942810@gmail.com>
2020-12-22 12:05:54 +0200

    add user verification (email)

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
     // email типа строка, обязательное и чтобы было уникальное - unique: true
     email: {type: String, required: true, unique: true},
     // password - строка,обязательное и так как пароль может быть одинаковым для РАЗНЫХ пользоваьлей мы не указываем unique: true
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    // у каждого пользователя будет свой массив[] ссылок. Тип будет айди который определён в mongoDB. ref - к какой колекции мы привязываемся
+    // 'Link' модель которую создам
+    links: [{type: Types.ObjectId, ref: 'Link'}]
 })
 
 module.exports = model('User', schema)
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
@@ -7,6 +8,12 @@ router.post('/register', async (req, res) => {
 
         const {email, password} = req.body
 
+        // логика регистрации (если уже есть такой пользователь то должны выдать ошибку и не регистрировать пользователя)
+        // проходим по модели пользователя findOne({email}) по email и если такой пользователь уже есть выдаём ошибку
+        const candidate = await User.findOne({email: email})
+        if (candidate) {
+           return res.status(400).json({ message: 'This user already exists'})
+        }
 
     } catch (e) {
         res.status(500).json({message: 'Something\'s wrong... Try again'})
