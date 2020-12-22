cfc2a5677b305b63599bd066e8568087ad12df2e
Artem <rozen19942810@gmail.com>
2020-12-22 11:34:08 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            // эти параметры нужны чтобы наш connect успешно работал
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT} ...`))
+    } catch (e) {
+        console.log('Server error', e.message)
+        // для того чтобы выйти из процесса node.js (завершим процесс в случае если пошло что-то нетак)
+        process.exit(1)
+    }
+}
+
+
+start()
